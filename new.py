27751d6b2812fe237cd728f5464e4bb3ad3f2ac1
rajendrakanna <rajendrakaivemula@gmail.com>
2025-04-23 10:20:36 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from agno.agent import Agent
+from agno.media import Image
+from agno.models.anthropic.claude import Claude
+from agno.tools.duckduckgo import DuckDuckGoTools
+
+agent = Agent(
+    model=Claude(id="claude-3-5-sonnet-20241022", api_key="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"),
+    tools=[DuckDuckGoTools()],
+    markdown=True
+)
+
+user_path = input("Enter the path to the image file: ")
+
+image_path = Path(__file__).parent.joinpath(user_path)
+
+# Read the image file content as bytes
+image_bytes = image_path.read_bytes()
+
+agent.print_response(
+    "Analyze the Image to find the skin color/skin tone of the person in the image and Recommend a suitable outfit for the person.",
+    images=[
+        Image(content=image_bytes),
+    ],
+    stream=True,
+)
